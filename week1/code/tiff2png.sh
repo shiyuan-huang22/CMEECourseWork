0fd7f35b7cb5111d8d5d9e1b3d5178f81efc0746
shiyuan <sh422@ic.ac.uk>
2022-10-13 20:32:20 +0100

    Modified version of tiff2png

--- week1/code/tiff2png.sh
+++ week1/code/tiff2png.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+# Author: Shiyuan sh422@ic.ac.uk
+# Script: tiff2png.sh
+# Description: Convert tiff to png
+# Arguements: none
+# Date: Oct.2022
 
-for f in *.tif;
-    do 
-        echo "Converting $f";
-        convert "$f"  "$(basename "$f" .tif).png";
-    done
+
+if [ -z "$1" ] ; then #Check if user entered a file  
+	echo "Please enter an image name!"
+else
+	if [ -f "$1" ] ; then #If user enter a file and it exists
+    
+	    if [[ $1 == *.tif ]] ; then #Check if the entered file is an image	
+		
+			echo "converting $1"
+			convert "$1" "${1%tif}png" 
+			echo "Done!"
+		else
+			echo "Input is not a tiff image"
+		fi
+	else 
+        echo "File does not exist"
+    fi
+
+fi
