bf4db86ed3866c5268907dffc03a15cb88d53670
shiyuan <sh422@ic.ac.uk>
2022-10-13 20:34:56 +0100

    modified version of concatenatetwofiles

--- week1/code/ConcatenateTwoFiles.sh
+++ week1/code/ConcatenateTwoFiles.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-if [ ! -f "$1" -o ! -f "$2" ];then
-  echo "error"
-  exit
-else
-  cat $1 > all.txt
-  cat $2 >> all.txt
+# Author: Shiyuan sh422@ic.ac.uk
+# Script: ConcatenateTwoFiles.sh
+# Description: Concatenate the contents of two files
+# Arguements: 1 + 2 -> 3
+# Date: Oct.2022
+
+
+if [ -z "$1" || -z "$2" ];then
+  echo "Please enter two files"
+elif [ -z "$3" ];then
+  echo "Please enter a name for your merged file"
+else  
+  cat $1 > $3
+  cat $2 >> $3
   echo "Merged File is"
-  cat all.txt
+  cat $3
 fi
+exit
